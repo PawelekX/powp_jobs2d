fa923e16325a9b829fb6d76e2e58e3b66497d7a8
PawelekX <paterpawe@gmail.com>
2025-11-29 11:41:03 +0100

    fix: use getDrawerController method from DrawerFeature in DrawerPanelControllerAdapter to drawing figure in the app view

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerPanelControllerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerPanelControllerAdapter.java
@@ -4,15 +4,17 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DrawerPanelControllerAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawerPanelControllerAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private DrawPanelController drawPanelController;
 
 	public DrawerPanelControllerAdapter() {
-		super();
+		this.drawPanelController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,7 +29,7 @@ public class DrawerPanelControllerAdapter extends DrawPanelController implements
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		this.drawPanelController.drawLine(line);
 		setPosition(x, y);
 	}
 
