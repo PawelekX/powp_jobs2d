7cc902ceadb14db63e522a434c7ce74785db4729
PawelekX <paterpawe@gmail.com>
2025-12-03 16:02:33 +0100

    refactor: rename adapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawPanelAdapter.java
@@ -9,11 +9,11 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class DrawerPanelControllerAdapter implements Job2dDriver {
+public class DrawPanelAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 	private DrawPanelController drawPanelController;
 
-	public DrawerPanelControllerAdapter() {
+	public DrawPanelAdapter() {
 		this.drawPanelController = DrawerFeature.getDrawerController();
 	}
 
